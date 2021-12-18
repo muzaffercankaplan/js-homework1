9be753d8bae9a1d5a310fca3f6fe9d0f943a3523
muzafferckaplan <muzafferckaplan@gmail.com>
2021-12-18 09:48:11 +0300

    finish

---
+++ js.js
@@ -0,0 +1,42 @@
+
+
+let userName = document.querySelector("#name")
+let time = document.querySelector("#time")
+let person = prompt("adınızı giriniz")
+
+
+// getDay komutu rakam olarak geri dönüyor, o yüzden burda bu arrayı tanımladım.
+
+let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"] 
+
+setInterval(function myTimer () {
+    const now = new Date()
+    const hours = now.toLocaleTimeString()
+    const day = days[now.getDay()]
+    time.innerHTML = `Saat: ${hours} ve günlerden ${day}`
+},1000);
+
+// prompt alınan bilgiye göre aşagıdaki komut çalışıyor.
+
+(person != "") ? userName.innerHTML = `Merhaba  ${person}!  Hoş geldin!` : userName.innerHTML = `Hoşgeldin "Yabancı"`
+
+// arka plan renk değiştirmeye başlama ve durma
+
+let start = document.querySelector("#start") //baslamatma butonu
+let stop = document.querySelector("#stop") // durdurma butonu
+let body = document.querySelector("body") // body seçimi
+
+
+
+start.onclick =function ()  {
+    myInterval = setInterval(function() {
+    let color1 = Math.floor(Math.random()* 255)
+    let color2 = Math.floor(Math.random()* 255)
+    let color3 = Math.floor(Math.random()* 255)
+    body.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;},100)
+    // durdurmak için stop.onclick fonksiyonunu yazdım.
+    stop.onclick =function(){
+        clearInterval(myInterval);
+        body.style.backgroundColor = "black"
+    }
+}
